fa8fab2363f9e6b4b1b964f6e0e897908f05393f
Andrey <andreyvasconchaves@gmail.com>
2021-03-14 18:21:36 -0300

    teste dos casos de uso

---
+++ src/test/java/com/qualidade/qualidade/CtrlRUTest.java
@@ -0,0 +1,38 @@
+package com.qualidade.qualidade;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlRUTest {
+
+	@Test
+	void testcriarReceita() {
+		assert(true);
+	}
+
+	@Test
+	void testcadastrarDadosReceita() {
+		assert(true);
+	}
+	
+	@Test
+	void testcadastrarDadosIngrediente() {
+		assert(true);
+	}
+
+	@Test
+	void testincluirReceita() {
+		assert(true);
+	}
+
+	@Test
+	void testobterListaReceitas() {
+		assert(true);
+	}
+
+}
---
+++ src/test/java/com/qualidade/qualidade/ListaDeReceitaTest.java
@@ -0,0 +1,42 @@
+package com.qualidade.qualidade;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+
+@SpringBootTest
+class ListaDeReceitaTest {
+
+	@Test
+	void deveCriarReceita() throws Exception{
+		throw new NotImplementedException("Nao implementada");
+	}
+
+	@Test
+	void deveCriarIngrediente() throws Exception{
+		throw new NotImplementedException("Nao implementada");
+	}
+
+	@Test
+	void deveCadastrarDadosIngrediente() throws Exception{
+		throw new NotImplementedException("Nao implementada");
+	}
+
+	@Test
+	void deveCadastrarDadosReceita() throws Exception{
+		throw new NotImplementedException("Nao implementada");
+	}
+
+	@Test
+	void deveObterReceita() throws Exception{
+		throw new NotImplementedException("Nao implementada");
+	}
+	
+
+
+}
