9bb62b7a82dc9337ddfdb44cc10b412082db39d3
Jean Carlo Sanchuki Filho <jean.sanchuki@gmail.com>
2021-03-17 02:03:30 -0300

    Added tests for 'Ingrediente' and 'Receita'

--- src/test/java/com/qualidade/qualidade/ReceitaTest.java
+++ src/test/java/com/qualidade/qualidade/ReceitaTest.java
@@ -1,14 +1,75 @@
 package com.qualidade.qualidade;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReceitaTest {
+	
+	private TipoID id;
+	private ItemEstoque item;
+	private Receita rec; 
 
-
+	@BeforeEach
+	void setUp() 
+	{
+		id = new TipoID(1);
+		rec = new Receita(id, "receita_a", "modo");
+		item = new ItemEstoque(id, "produto_a", 10, "mg", 15f);
+	}
+	
+	@Test
+	void deveAdicionarNovoIngrediente() 
+	{
+		assertTrue(rec.getIngredientes().isEmpty());
+		rec.adicionarIngrediente(item, 13, "kf");		
+		assertFalse(rec.getIngredientes().isEmpty());
+		assertEquals(id.get(), rec.getIngredientes().remove(0).getID().get());
+	}
+	
+	@Test
+	void deveRemoverIngrediente() 
+	{
+		assertNotNull(rec.getIngredientes());
+		rec.adicionarIngrediente(item, 43, "kg");
+		rec.adicionarIngrediente(item, 231, "mg");
+		rec.removerIngredientes();
+		assertTrue(rec.getIngredientes().isEmpty());
+	}
+	
+	@Test
+	void deveManterEstoqueSemIngrediente() 
+	{
+		rec.atualizarEstoque("adicionar", 100);
+		assertTrue(rec.ingredientesDisponiveisNoEstoque(99));
+		assertTrue(rec.ingredientesDisponiveisNoEstoque(1));
+		assertFalse(rec.ingredientesDisponiveisNoEstoque(125));		
+	}
+	
+	@Test
+	void deveManterEstoqueComIngrediente()
+	{
+		rec.adicionarIngrediente(item, 10, "mg");
+		rec.atualizarEstoque("adicionar", 100);
+		assertTrue(rec.ingredientesDisponiveisNoEstoque(99));
+		assertTrue(rec.ingredientesDisponiveisNoEstoque(1));
+		assertFalse(rec.ingredientesDisponiveisNoEstoque(105));	
+	}
+	
+	@Test
+	void deveCalcularCusto()
+	{
+		rec.adicionarIngrediente(item, 10, "kf");	
+		assertEquals(2500 ,rec.obterCusto(25));
+	}
+	
+	
 }
